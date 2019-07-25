4912927586f50ab045ab40dd73bbb76e8efc0d28
Aman Gupta <aman.gupta0107@gmail.com>
2019-07-25 20:18:55 +0530

    module

--- app.js
+++ app.js
@@ -26,4 +26,35 @@ const UICtrl = (function() {
 	};
 })();
 
-UICtrl.callChangeText();
+// UICtrl.callChangeText();
+// // UICtrl.changeText();
+
+// console.log(UICtrl.text);
+
+//REVEALING MODULE PATTERN
+
+//instead of returning public function , it mapped private function u want to reveal.
+
+const ItemCtrl = (function() {
+	const data = [];
+
+	function add(item) {
+		data.push(item);
+		console.log('Item added');
+	}
+
+	function get(id) {
+		return data.find(item => {
+			return item.id === id;
+		});
+	}
+
+	return {
+		add: add,
+		get: get
+	};
+})();
+
+ItemCtrl.add({ id: 1, name: 'John' });
+ItemCtrl.add({ id: 2, name: 'Mark' });
+console.log(ItemCtrl.get(2));
