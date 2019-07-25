df3bb32cf4b70b10cb0929f04c0a2cdd9a4aa9de
Aman Gupta <aman.gupta0107@gmail.com>
2019-07-25 10:29:24 +0530

    first

---
+++ app.js
@@ -0,0 +1,29 @@
+// Basic structure
+
+// (function() {
+//   // Declare private vars and functions
+
+//   return {
+//     // Declare public var and functions
+//   }
+// })();
+
+//STANDARD MODULE PATTERN
+const UICtrl = (function() {
+	let text = 'Hello';
+
+	const changeText = function() {
+		const element = document.querySelector('h1');
+		element.textContent = text;
+	};
+
+	return {
+		callChangeText: function() {
+			changeText();
+
+			console.log(text);
+		}
+	};
+})();
+
+UICtrl.callChangeText();
